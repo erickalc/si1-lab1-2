a84773bb89333bfe66d6f510d49278eb56f93f76
Stenio Elson <stenio.araujo@ccc.ufcg.edu.br>
2014-05-23 03:21:12 -0300

    Refatorando

--- README.txt
+++ README.txt
@@ -2,13 +2,13 @@
 
 Diretório destinado a disciplina Sistema da Informação I, com especial atenção ao Laboratório 01.
 
-Tarefas:
+Tarefas: OK
 	- Parte 1 - OK
         - Parte 2 - OK
 		- estatica-simples - OK
 		- estatica-css - OK
 		- estatica-bootstrap - OK
 	- Parte 3 - OK
-	- Parte 4
+	- Parte 4 - OK
 
 Todas as informações aqui contidas estão sujeitas a erros. Estou aprendendo.
--- dinamica/js/learn.js
+++ dinamica/js/learn.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// Ativia adicionar com enter
 	$(".add input").keypress(function(event){
 		if (event.keyCode == 13) {
 			$(".add button").click();
@@ -6,27 +7,30 @@ $(document).ready(function(){
 	});
 	
 	$(".add button").click(function(){
+		// Criando elementos
 		var input = $(".add input"),
 		    item = $("<li></li>"),
 		    itemContent = $("<span></span>"),
 		    itemValue = input.val();
 		    colors = ["label-primary", "label-success", "label-info", "label-warning", "label-danger"]
 		
+		
+		// Verifica se Existe algum caracter no campo
 		if (itemValue == "") {
 			$(".add").addClass("has-error");
 			input.focus();
 			return;
 		} $(".add").removeClass("has-error");
 		
-		item.append(itemContent);
-		
+		// Cria o item
 		itemContent.addClass("label").addClass(colors[new Date().getMilliseconds() % colors.length]).html(itemValue);
 		itemContent.append($(".defaultOptions").clone().removeClass("defaultOptions").addClass("options"));
+		item.append(itemContent);
 		
+		// Adicionar o item aos assunstos a rasgar
 		$(".rasgar").append(item);
-		input.val("");
-		input.focus();
 		
+		//Adiciona os listeners para rasgado e remover no item
 		$(".scheduler-border .options a").click(function(event){
 			if($(event.target).closest("a").attr("href") == "#remove") {
 				$(event.target).closest("li").remove();
@@ -34,14 +38,17 @@ $(document).ready(function(){
 				$(event.target).closest("li").appendTo(".rasgados");
 			}
 		});
+		
+		// Limpa o campo
+		input.val("");
+		input.focus();
 	});
 	
-	$(".rasgar, .rasgados").disableSelection();
-	
+	// Configura o Drag and Drop do jQueryUI
+	$(".rasgar, .rasgados").disableSelection();	
 	$(".rasgar").sortable({
 		connectWith: ".rasgados"
-	});
-	
+	});	
 	$(".rasgados").sortable({
 		connectWith: ".rasgar"
 	});
